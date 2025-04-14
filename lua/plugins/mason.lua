0e112948c8378684cc3d5a3d45707fdc3019e5d4
sillypoise <somewhatjose@gmail.com>
2025-04-13 23:27:17 -0500

    add mason config

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,22 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "biome",
+      "eslint_d",
+      "eslint-lsp",
+      -- "hclfmt", -- go not found
+      "ltex-ls",
+      "markdown-oxide",
+      "mdslw",
+      -- "nil", -- cargo not found
+      "stylua",
+      "shfmt",
+      "tailwindcss-language-server",
+      "terraform-ls",
+      "typescript-language-server",
+      "prettierd",
+      "jq",
+    },
+  },
+}
