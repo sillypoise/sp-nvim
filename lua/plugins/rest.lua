c5e0fbcd31a0a7f7dfe491fc83359eb7343a6b3e
sillypoise <somewhatjose@gmail.com>
2025-09-20 13:43:29 -0500

    rest | update to include explicit LuaRocks dep

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,3 +1,12 @@
 return {
   "rest-nvim/rest.nvim",
+  dependencies = {
+    "nvim-nio/nvim-nio",
+    "j-hui/fidget.nvim",
+  },
+  ft = { "http", "rest" },
+  rocks = {
+    "mimetypes",
+    "xml2lua",
+  },
 }
